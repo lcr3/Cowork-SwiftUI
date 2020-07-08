a50b085167a59f17cc25b0c417a98415564665dc
lassic-okano <okano@lassic.co.jp>
2020-07-08 09:15:06 +0900

    Initial Commit

---
+++ Cowork-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cowork-SwiftUI
+//
+//  Created by ryookano on 2020/07/08.
+//  Copyright © 2020 lcr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
